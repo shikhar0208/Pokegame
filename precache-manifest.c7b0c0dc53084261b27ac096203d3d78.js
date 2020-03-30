8f3dc96e88b9bd8ca7ba8808a7b4ce9908004ee1
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2020-03-30 19:05:54 +0530

    Updates

---
+++ precache-manifest.c7b0c0dc53084261b27ac096203d3d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d58f9af8becb51dcc233bd0a65a634",
+    "url": "/Pokegame/index.html"
+  },
+  {
+    "revision": "7db67785dfa642675c1a",
+    "url": "/Pokegame/static/css/main.43f72f1d.chunk.css"
+  },
+  {
+    "revision": "f6797edc8011a7eba12a",
+    "url": "/Pokegame/static/js/2.e6b5e241.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokegame/static/js/2.e6b5e241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db67785dfa642675c1a",
+    "url": "/Pokegame/static/js/main.c997ae42.chunk.js"
+  },
+  {
+    "revision": "45b77adeeb24bc35a16a",
+    "url": "/Pokegame/static/js/runtime-main.a41437ae.js"
+  }
+]);
